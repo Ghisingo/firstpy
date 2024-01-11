e0a58bec4f90b484b92adc098feb849b848a2a5d
Ghisingo <aasish1ghising@gmail.com>
2024-01-11 10:21:31 +0545

    add loop for multiple caculations

--- first.py
+++ first.py
@@ -1,15 +1,4 @@
-print ("""
-       
-       Welcome to calculator!
-       Enter the operaiton you want to proceed:
-       1) Addition
-       2) Subtraction
-       3) Division
-       4) Multiplication
-       5) Exit 
 
-
-""")
 
 def take_input():
     first_num = int(input("Enter first number: "))
@@ -33,43 +22,41 @@ def mul():
     num1, num2 = take_input()
     return (num1 * num2)
 
-choice = int(input("Enter your choice:"))
+
+
+while True:
+    print ("""
+Welcome to calculator!
+Enter the operaiton you want to proceed:
+1) Addition
+2) Subtraction
+3) Division
+4) Multiplication
+5) Exit 
+        """)
+
+    choice = int(input("Enter your choice:"))
+
+    if choice == 1:
+     print("The sum is : ", sum())
+
+    elif choice == 2:
+     print("The difference is : ", sub())
+
+    elif choice == 3:
+     print("The result of division is : ", div())
+
+    elif choice == 4:
+     print("The result of multiplication is : ", mul())
+
+    elif choice == 5:
+     print ("Thank you!")
+     exit()
     
-if choice == 1:
-    print("The sum is : ", sum())
+    else:
+        print("Please enter the number between 1-5")
+ 
 
-elif choice == 2:
-    print("The difference is : ", sub())
-
-elif choice == 3:
-    print("The result of division is : ", div())
-
-elif choice == 4:
-    print("The result of multiplication is : ", mul())
-
-elif choice == 5:
-    print ("Thank you!")
-    exit()
-
-else:
-    print("choose form 1 to 4")
-
-
-# again = input(print ("Again? y or n: "))
-# y = 1
-# n = 0 
-# if again == y:
-#     print ("""
-#        Enter the operaiton you want to proceed:
-#        1) Addition
-#        2) Subtraction
-#        3) Division
-#        4) Multiplication
-#        5) Exit 
-# """, take_input())
-
-# else:
-#     print("ok bye")
 
 
 
